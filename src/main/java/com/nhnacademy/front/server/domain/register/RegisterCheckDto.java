2d8892a131c1bb0d5b08de4d21499bbcf4040a6e
AoiTuNa <bestspark123@naver.com>
2024-04-08 11:25:20 +0900

    domain validation 추가

--- src/main/java/com/nhnacademy/front/server/domain/register/RegisterCheckDto.java
+++ src/main/java/com/nhnacademy/front/server/domain/register/RegisterCheckDto.java
@@ -2,6 +2,7 @@ package com.nhnacademy.front.server.domain.register;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.validation.annotation.Validated;
 
 /**
  * 회원가입 폼에서 유저가 입력한 정보를 저장하기 위해 사용하는 domain객체 입니다!
@@ -10,6 +11,7 @@ import lombok.Getter;
  */
 @Getter
 @AllArgsConstructor
+@Validated
 public class RegisterCheckDto {
   private String id;
   private String password;
