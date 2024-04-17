18b2780dd7285a2febfb7a0b7adee2c3bdc559ea
AoiTuNa <bestspark123@naver.com>
2024-04-17 15:31:42 +0900

    토큰 secure 규칙에 추가 ㄴ /login /register 외의 전체 로직에 토큰의 만료를 검사하는 필터를 작동하게 설정해봄

--- src/main/java/com/nhnacademy/front/server/config/SecureConfig.java
+++ src/main/java/com/nhnacademy/front/server/config/SecureConfig.java
@@ -1,24 +1,31 @@
 package com.nhnacademy.front.server.config;
 
+import com.nhnacademy.front.server.filter.TokenExpirationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecureConfig {
+    private final TokenExpirationFilter tokenExpirationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 //.cors()
                 //.and()
+                //Todo 다하면 csrf disable 해제하기!
                 .csrf().disable()
                 .httpBasic().disable()
                 .formLogin().disable()
                 .headers().frameOptions().disable()
                 .and()
+                .addFilterBefore(tokenExpirationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .antMatchers("/login","/register","/js/***","/css/**", "/images/**").permitAll()
