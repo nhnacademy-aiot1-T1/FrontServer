9d7d5f58ea25f44542845f9d30200311bf8fafc3
jki12 <sjak0227@naver.com>
2024-04-24 13:52:57 +0900

    feat: restTemplate timeout 설정 추가, Object Mapper 추가

--- src/main/java/com/nhnacademy/front/server/config/WebConfig.java
+++ src/main/java/com/nhnacademy/front/server/config/WebConfig.java
@@ -1,43 +1,38 @@
 package com.nhnacademy.front.server.config;
 
-import com.nhnacademy.front.server.adapter.AuthAdapter;
-import com.nhnacademy.front.server.adapter.impl.AuthAdapterImpl;
-import com.nhnacademy.front.server.filter.TokenExpirationFilter;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
 
 
 /**
- * 스프링 부트를 활용하기 위한 wevConfig 파일입니다!
+ * 스프링 부트를 활용하기 위한 webConfig 파일입니다!
  * @author AoiTuNa
  * @version 1.1
- * @see #restTemplate()
  */
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     /**
      * bean으로 등록된 restTemplate 객체입니다!
      * @return 설정된 빌더를 리턴합니다!
      */
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+            .build();
     }
 
-
-    /**
-     * 모든 페이지에 접근하기전에 클라이언트의 쿠키에 인증 토큰의 유무를 확인하는 filter의 규칙을 추가합니다!<br/>
-     * 모든 요청에서 토큰의 유무를 검증합니다!
-     * @param authService 토큰의 유무를 검증하기 위한 service 객체입니다!
-     * @return 필터규칙을 리턴합니다!
-     */
-
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 }
