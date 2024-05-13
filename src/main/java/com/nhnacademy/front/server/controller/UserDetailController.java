51ac365bf836e7ff50c7b10dc42baac68a14e7b4
chanhwiim <chanhwiim459@gmail.com>
2024-05-13 14:58:58 +0900

    chore: change class name

--- src/main/java/com/nhnacademy/front/server/controller/UserDetailController.java
+++ src/main/java/com/nhnacademy/front/server/controller/UserDetailController.java
@@ -1,23 +1,16 @@
 package com.nhnacademy.front.server.controller;
 
 import com.nhnacademy.front.server.dto.UserDetailDto;
-import com.nhnacademy.front.server.dto.UserRole;
-import com.nhnacademy.front.server.service.UserService;
-import javax.ws.rs.Path;
+import com.nhnacademy.front.server.service.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,7 +18,7 @@ import org.springframework.web.servlet.view.RedirectView;
 @RequestMapping("/mypage")
 public class UserDetailController {
 
-  private final UserService userService;
+  private final UserDetailService userService;
 
   /**
    *
@@ -50,6 +43,6 @@ public class UserDetailController {
 
     log.info(" is :{}", updateUser);
 
-    return "dashboard";
+    return "users-edit";
   }
 }
