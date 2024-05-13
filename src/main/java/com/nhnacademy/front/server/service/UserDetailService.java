99f7b520609a140fae58da8814c8d524d6a60cad
chanhwiim <chanhwiim459@gmail.com>
2024-05-13 14:58:46 +0900

    chore: change class name

--- src/main/java/com/nhnacademy/front/server/service/UserDetailService.java
+++ src/main/java/com/nhnacademy/front/server/service/UserDetailService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserDetailService {
 
   private final UserAdaptor userAdaptor;
 
