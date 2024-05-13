5e60c8085eb37b0eb0a9b4beb1cd549f824fdc91
chanhwiim <chanhwiim459@gmail.com>
2024-05-13 10:29:06 +0900

    feat: 메소드 분리, private method return type 변경, post 형식 변경. 예외 처리 추가

--- src/main/java/com/nhnacademy/front/server/adapter/impl/UserAdaptorImpl.java
+++ src/main/java/com/nhnacademy/front/server/adapter/impl/UserAdaptorImpl.java
@@ -1,10 +1,13 @@
 package com.nhnacademy.front.server.adapter.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.common.dto.CommonResponse;
 import com.nhnacademy.front.server.adapter.UserAdaptor;
 import com.nhnacademy.front.server.dto.UserDetailDto;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -17,26 +20,26 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class UserAdaptorImpl implements UserAdaptor {
 
   private final RestTemplate restTemplate;
 
   private static final MediaType CONTENT_TYPE = MediaType.APPLICATION_JSON;
-  private static final String GET_USER_DETAIL_URL = "https://run.mocky.io/v3/33acb19b-6557-44f6-914c-4a8ef12b0fa4";
-  private static final String POST_USER_DETAIL_URL = "";
+  private static final String GET_USER_DETAIL_URL = "https://run.mocky.io/v3/1dc226c9-8189-49ef-8bbe-28616b6d2f1f";
+  private static final String POST_USER_DETAIL_URL = "https://run.mocky.io/v3/cfd28bcd-01f2-4de3-9db2-53253d403a71";
+  private static final String JSON_PARSING_ERROR_MESSAGE = "JSON parsing error";
 
   @Override
-  public CommonResponse<UserDetailDto> getUserDetail(Long userId) {
-    HttpEntity<String> request = setupHttpHeaders();
+  public CommonResponse<UserDetailDto> getUserDetail(Long id) {
+    HttpHeaders headers = setupHttpHeaders();
+    HttpEntity<String> request = new HttpEntity<>(headers);
 
-    ResponseEntity<CommonResponse<UserDetailDto>> exchange = restTemplate.exchange(
-        GET_USER_DETAIL_URL,
-        HttpMethod.GET,
-        request,
-        new ParameterizedTypeReference<>() {
-        });
+    ResponseEntity<CommonResponse<UserDetailDto>> exchange = restTemplateExchange(
+        GET_USER_DETAIL_URL, HttpMethod.GET, request);
 
+    log.error("it is :{}", exchange.getBody());
     if (!exchange.getStatusCode().equals(HttpStatus.OK)) {
       throw new ResponseStatusException(exchange.getStatusCode());
     }
@@ -44,27 +47,50 @@ public class UserAdaptorImpl implements UserAdaptor {
   }
 
   @Override
-  public CommonResponse<UserDetailDto> updateUserDetail(UserDetailDto userDetailDto) {
-    HttpEntity<String> request = setupHttpHeaders();
+  public CommonResponse<UserDetailDto> updateUserDetail(Long id, UserDetailDto userDetailDto) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    String requestBody = null;
 
-    ResponseEntity<CommonResponse<UserDetailDto>> exchange = restTemplate.exchange(
-        POST_USER_DETAIL_URL,
-        HttpMethod.POST,
-        request,
-        new ParameterizedTypeReference<>() {
-        });
+    try {
+      requestBody = objectMapper.writeValueAsString(userDetailDto);
+    } catch (JsonProcessingException e) {
+      log.error(JSON_PARSING_ERROR_MESSAGE + ":{}", e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+          JSON_PARSING_ERROR_MESSAGE);
+    }
 
+    HttpEntity<String> request = new HttpEntity<>(requestBody, setupHttpHeaders());
+
+    ResponseEntity<CommonResponse<UserDetailDto>> exchange;
+    try {
+      exchange = restTemplateExchange( POST_USER_DETAIL_URL, HttpMethod.POST, request);
+    } catch (ResponseStatusException e) {
+      log.error("RestTemplate exchange error :{}",e.getMessage());
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while posting user detail");
+    }
+
+    log.info("it is :{}", exchange.getBody());
     if (!exchange.getStatusCode().equals(HttpStatus.OK)) {
       throw new ResponseStatusException(exchange.getStatusCode());
     }
     return exchange.getBody();
   }
 
-  private HttpEntity<String> setupHttpHeaders() {
+  private HttpHeaders setupHttpHeaders() {
     HttpHeaders httpHeaders = new HttpHeaders();
     httpHeaders.setContentType(CONTENT_TYPE);
     httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
 
-    return new HttpEntity<>(httpHeaders);
+    return httpHeaders;
+  }
+
+  private ResponseEntity<CommonResponse<UserDetailDto>> restTemplateExchange(String url,
+      HttpMethod httpMethod, HttpEntity<String> request) {
+    return restTemplate.exchange(
+        url,
+        httpMethod,
+        request,
+        new ParameterizedTypeReference<>() {
+        });
   }
 }
