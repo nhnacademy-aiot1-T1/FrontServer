ae62b7f95a5bfa56d3e7465be3f43bd40189c933
chanhwiim <chanhwiim459@gmail.com>
2024-05-13 10:27:45 +0900

    feat: add method param

--- src/main/java/com/nhnacademy/front/server/adapter/UserAdaptor.java
+++ src/main/java/com/nhnacademy/front/server/adapter/UserAdaptor.java
@@ -5,7 +5,7 @@ import com.nhnacademy.front.server.dto.UserDetailDto;
 
 public interface UserAdaptor {
 
-  CommonResponse<UserDetailDto> getUserDetail(Long userId);
+  CommonResponse<UserDetailDto> getUserDetail(Long id);
 
-  CommonResponse<UserDetailDto> updateUserDetail(UserDetailDto userDetailDto);
+  CommonResponse<UserDetailDto> updateUserDetail(Long id, UserDetailDto userDetailDto);
 }
