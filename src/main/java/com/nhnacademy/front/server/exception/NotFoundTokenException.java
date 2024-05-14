ca5ad7df51af8fa274337d0fca8e3ebaf332c60f
jki12 <sjak0227@naver.com>
2024-05-14 16:03:11 +0900

    fix: UnauthorizedException을 상속받도록 수정

--- src/main/java/com/nhnacademy/front/server/exception/NotFoundTokenException.java
+++ src/main/java/com/nhnacademy/front/server/exception/NotFoundTokenException.java
@@ -1,12 +1,11 @@
 package com.nhnacademy.front.server.exception;
 
-/**
- * 로그아웃시 토큰을 찾지 못했을 때 발생하는 예외입니다!
- * @author AoiTuNa
- * @version 1.0
- */
-public class NotFoundTokenException extends RuntimeException {
+public class NotFoundTokenException extends UnauthorizedException {
     public NotFoundTokenException(String message) {
         super(message);
+    }
+
+    public NotFoundTokenException() {
+        super();
     }
 }
