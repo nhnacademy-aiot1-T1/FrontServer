6633a692070d3930154538ff8042247e2f9a445d
jki12 <sjak0227@naver.com>
2024-05-16 21:11:02 +0900

    feat: auth service 인터페이스 추가

---
+++ src/main/java/com/nhnacademy/front/server/service/AuthService.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.front.server.service;
+
+import com.nhnacademy.front.server.dto.LoginResponseDto;
+import com.nhnacademy.front.server.dto.UserLoginRequestDto;
+import com.nhnacademy.front.server.dto.register.RegisterRequestDto;
+
+public interface AuthService {
+    LoginResponseDto login(UserLoginRequestDto requestDto);
+
+    LoginResponseDto oauthLogin(String authCode);
+
+    void logout(String accessToken);
+
+    void registerUser(RegisterRequestDto registerRequestDto);
+
+    String requestTokenRefresh(String accessToken);
+}
