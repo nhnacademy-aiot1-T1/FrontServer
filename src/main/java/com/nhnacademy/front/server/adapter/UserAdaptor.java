3224264325549148cbee63c573e62f5a851ffcef
chanhwiim <chanhwiim459@gmail.com>
2024-05-17 10:54:49 +0900

    feat : add users

--- src/main/java/com/nhnacademy/front/server/adapter/UserAdaptor.java
+++ src/main/java/com/nhnacademy/front/server/adapter/UserAdaptor.java
@@ -2,10 +2,13 @@ package com.nhnacademy.front.server.adapter;
 
 import com.nhnacademy.common.dto.CommonResponse;
 import com.nhnacademy.front.server.dto.UserDetailDto;
+import java.util.List;
 
 public interface UserAdaptor {
 
   CommonResponse<UserDetailDto> getUserDetail(Long id);
+
+  CommonResponse<List<UserDetailDto>> getUsers();
 
   CommonResponse<UserDetailDto> updateUserDetail(Long id, UserDetailDto userDetailDto);
   
