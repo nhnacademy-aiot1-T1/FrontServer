cad4844de375ad00c12332b6af98e50aa733deb4
chanhwiim <chanhwiim459@gmail.com>
2024-05-21 09:35:12 +0900

    test: 임시

--- src/test/java/com/nhnacademy/front/server/controller/UserDetailControllerTest.java
+++ src/test/java/com/nhnacademy/front/server/controller/UserDetailControllerTest.java
@@ -2,6 +2,7 @@ package com.nhnacademy.front.server.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.nhnacademy.front.server.dto.UserDetailDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,9 +17,30 @@ class UserDetailControllerTest {
   private UserDetailController userDetailController;
 
   @Test
-  void userDetail() {
+  void getUserDetail() {
     Model model = new ExtendedModelMap();
 
     userDetailController.getUserDetail(model, 1L);
+  }
+
+  @Test
+  void getUsers() {
+    Model model = new ExtendedModelMap();
+    userDetailController.getUsers(model);
+  }
+
+  @Test
+  void updateUserDetail() {
+    Model model = new ExtendedModelMap();
+
+    UserDetailDto userDetailDto = new UserDetailDto();
+
+    userDetailController.updateUserDetail(userDetailDto,1L);
+  }
+
+  @Test
+  void deleteUserDetail() {
+    Model model = new ExtendedModelMap();
+    userDetailController.deleteUserDetail(1L);
   }
 }
