d3d6df3e7eecd89d5c3d0f4ae9f3bf392e9bf3fa
jki12 <sjak0227@naver.com>
2024-05-22 13:39:50 +0900

    fix: AllArgsConstructor 제거 후 setter 추가

--- src/main/java/com/nhnacademy/front/server/dto/register/RegisterRequestDto.java
+++ src/main/java/com/nhnacademy/front/server/dto/register/RegisterRequestDto.java
@@ -1,18 +1,13 @@
 package com.nhnacademy.front.server.dto.register;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /**
  * gateway server를 통해 api server로 회원가입한 유저의 정보를 보내기위해 만들어진 domain객체 입니다!
- * @author AoiTuNa
- * @version 1.0
  */
 @Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class RegisterRequestDto {
   private String userId;
   private String password;
