30b3f296e438296d5e21c131a507569aca6a52c1
jki12 <sjak0227@naver.com>
2024-06-03 13:44:14 +0900

    fix: date time format 추가

--- src/main/java/com/nhnacademy/front/server/config/CustomDeserializer.java
+++ src/main/java/com/nhnacademy/front/server/config/CustomDeserializer.java
@@ -3,26 +3,28 @@ package com.nhnacademy.front.server.config;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CustomDeserializer {
 
-  private CustomDeserializer() {
-
-  }
-
   @JsonComponent
-  public static class CustomLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
-
+  public static class CustomLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime>{
     @Override
-    public LocalDateTime deserialize(JsonParser jsonParser,
-        DeserializationContext deserializationContext) throws IOException {
-      return LocalDateTime.parse(jsonParser.getValueAsString(),
-          DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+      DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+      DateTimeFormatter formatter = builder.appendPattern("yyyy-MM-dd HH:mm:ss")
+              .appendPattern(String.valueOf(DateTimeFormatter.ISO_DATE_TIME))
+              .toFormatter();
+
+      return LocalDateTime.parse(jsonParser.getValueAsString(), formatter);
     }
   }
 }
