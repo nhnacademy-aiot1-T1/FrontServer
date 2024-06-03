e5e8cbed50d80773accd8deb76b5e9069fc9ca08
jki12 <sjak0227@naver.com>
2024-06-03 14:18:08 +0900

    fix: date time pattern 수정

--- src/main/java/com/nhnacademy/front/server/config/CustomDeserializer.java
+++ src/main/java/com/nhnacademy/front/server/config/CustomDeserializer.java
@@ -10,7 +10,6 @@ import org.springframework.boot.jackson.JsonComponent;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CustomDeserializer {
@@ -19,10 +18,7 @@ public class CustomDeserializer {
   public static class CustomLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime>{
     @Override
     public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-      DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
-      DateTimeFormatter formatter = builder.appendPattern("yyyy-MM-dd HH:mm:ss")
-              .appendPattern(String.valueOf(DateTimeFormatter.ISO_DATE_TIME))
-              .toFormatter();
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("[yyyy-MM-dd HH:mm:ss][yyyy-MM-dd'T'HH:mm:ss.SSS]");
 
       return LocalDateTime.parse(jsonParser.getValueAsString(), formatter);
     }
