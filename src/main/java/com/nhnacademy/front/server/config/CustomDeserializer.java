04571de91c4ab5f91beded1edbc662707350e8f6
jki12 <sjak0227@naver.com>
2024-06-03 13:30:22 +0900

    fix: date time format 추가

--- src/main/java/com/nhnacademy/front/server/config/CustomDeserializer.java
+++ src/main/java/com/nhnacademy/front/server/config/CustomDeserializer.java
@@ -8,6 +8,7 @@ import org.springframework.boot.jackson.JsonComponent;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 public class CustomDeserializer {
 
@@ -17,10 +18,14 @@ public class CustomDeserializer {
 
     @JsonComponent
     public static class CustomLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime>{
-
         @Override
         public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
-            return LocalDateTime.parse(jsonParser.getValueAsString() , DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+            DateTimeFormatter formatter = builder.appendPattern("yyyy-MM-dd HH:mm:ss")
+                    .appendPattern(String.valueOf(DateTimeFormatter.ISO_DATE_TIME))
+                    .toFormatter();
+
+            return LocalDateTime.parse(jsonParser.getValueAsString(), formatter);
         }
     }
 }
